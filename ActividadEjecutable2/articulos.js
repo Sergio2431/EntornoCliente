70c9f1ec3970ff3b5b48d035c1f742a771cf6fe5
WINMAC17\2daw <sergio_311096@hotmail.es>
2018-10-16 13:27:04 +0200

    subir fichero

--- ActividadEjecutable2/articulos.js
+++ ActividadEjecutable2/articulos.js
@@ -7,6 +7,18 @@
 
 const readline = require('readline-sync');
 
+class Autor {
+
+    constructor(nombre,apellidos,id_autor){
+        this.nombre = nombre;
+        this.apellidas = apellidos;
+        this.id_autor = id_autor;
+    }
+    getNombre(){
+        return this.titulo;
+    }
+}
+
 class ArtRevis {
 
     constructor(titulo,autor,n_pag,anyoPub,numMen,revistCien,tituRevis,editorial){
@@ -21,7 +33,7 @@ class ArtRevis {
      
     }
  
-    getMatricula(){
+    getTitulo(){
         return this.titulo;
     }
  
@@ -42,7 +54,7 @@ class ArtConf {
      
     }
  
-    getMatricula(){
+    getTitulo(){
         return this.titulo;
     }
  
@@ -59,22 +71,33 @@ class ArtConf {
 
     }
  
-    getMatricula(){
-        return this.autores;
+    getTitulo(){
+        return this.titulo;
     }
  
  } 
 
 let listaPubli = [];
-let salir = false;
-while(!salir){
+let atras = false;
+while(!atras){
    console.log('Bienvenidos al sistema de gestión de articulos');
+   console.log('0) Dar de alta un autor');
    console.log('1) Dar de alta un articulo');
    console.log('2) Dar de baja un articulo');
-   console.log('3) Buscar un articulo y mostrar información');
-   console.log('4) Salir del sistema');
+   console.log('3) Modificar una publicacion');
+   console.log('4) Busqueda de publicaciones');
+   console.log('8) Salir del sistema');
    let opcion=readline.questionInt('Por favor, seleccione una de estas opciones: ');
    
+   if(opcion===0){
+    //Dar de alta un Autor
+     let nombre=readline.question('Por favor, introduce el nombre del autor: ');
+     let apellidos=readline.question('Por favor, introduce los apellidos: ');
+     let id_autor=readline.question('Por favor, introduce el numero de ID: ');
+     let newAutor = new Autor(nombre,apellidos,id_autor);
+                listaPubli.push(newAutor);
+                console.log(listaPubli);
+   }
    if(opcion===1){
        //Seleccionar el tipo de Articulo
        let atras = false;
@@ -86,11 +109,11 @@ while(!salir){
            let opcion=readline.questionInt('Por favor, seleccione una de estas opciones: ');
            if(opcion===1){
                //Dar de alta el Articulo de Revista
-                let titulo=readline.question('Por favor, introduce una titulo: ');
-                let autor=readline.question('Por favor, introduce una autor: ');
+                let titulo=readline.question('Por favor, introduce un titulo: ');
+                let autor=readline.question('Por favor, introducñe un autor: ');
                 let n_pag=readline.question('Por favor, introduce un numero de paginas: ');
                 let anyoPub=readline.question('Por favor, introduce un anyo de publicacion: ');
-                let numMen=readline.question('Por favor, introduce un numero de menciones: ');
+                let numMen=readline.question('Por favor, introduce un numerño de menciones: ');
                 let tituRevis=readline.question('Por favor, introduce en que revista aparecio: ');
                 let editorial=readline.question('Por favor, introduce la editorial: ');
                 let newArtRevis = new ArtRevis(titulo,autor,n_pag,anyoPub,numMen,tituRevis,editorial);
@@ -191,23 +214,80 @@ while(!salir){
                 } if(opcion===4){
                     atras=true;
                 }
-      
-   }if(opcion===3){
-       //Buscar un Articulo Cientifico
-       let titulo = readline.question('Por favor introduce un titulo: ');
-       for(let ArtCient of listaPubli){
-           if(ArtCient.titulo===titulo){
-               console.log(ArtCient);
-               break;
-           }
-       }
+            }
+        }
+    if(opcion===3){
+       //Modificar publicaciones
+       let atras = false;
+       while(!atras){
+           console.log('1) Modificar un Articulo de Revista');
+           console.log('2) Modificar un Articulo de Conferencia');
+           console.log('3) Modificar una Patente');
+           console.log('4) Volver a las opciones');
+           let opcion=readline.questionInt('Por favor, seleccione una de estas opciones: ');
+            if(opcion===1){
+                //Modificar un Articulo Cientifico
+                let titulo = readline.question('Por favor introduce un titulo: ');
+                let encontrado = falso;
+                if (ArtRevis.title === titulo && ArtRevis.titulo === true) {
+                    console.log('Introduce los datos que desea modificar');
+                    let titulo = readline.question('Introduce el titulo: ');
+                    if (titulo === '')publications[i].setTitle(titulo);
+
+                    let autor = readline.question('Introduce el titulo: ');
+                    if (autor === '')publications[i].setTitle(autor);
+
+                    let n_pag = readline.question('Introduce el titulo: ');
+                    if (n_pag === '')publications[i].setTitle(n_pag);
+
+                    let anyoPub = readline.question('Introduce el titulo: ');
+                    if (anyoPub === '')publications[i].setTitle(anyoPub);
+
+                    let numMen = readline.question('Introduce el titulo: ');
+                    if (numMen === '')publications[i].setTitle(numMen);
+
+                    let revistCien = readline.question('Introduce el titulo: ');
+                    if (revistCien === '')publications[i].setTitle(revistCien);
+
+                    let tituRevis = readline.question('Introduce el titulo: ');
+                    if (tituRevis === '')publications[i].setTitle(tituRevis);
+
+                    let editorial = readline.question('Introduce el titulo: ');
+                    if (editorial === '')publications[i].setTitle(editorial);
+                }
+           
+            }
+    }
+}
+
+        if(opcion===4){
+            //Busqueda de las diferentes publicaciones
+            let autor = readline.question('Introduce el autor o pulsa enter para seguir: ');
+            let anyoPub = readline.question('Introduce el anyo de publicación o 0: ');
+            let tipo = readline.question('Introduce el tipo de publicacion o pulsa enter para seguir: ');
+            let busqueda = [];
+            let encontrado = false;
+
+            for (public of publications) { 
+                if (autor!='') {
+                    for (autor of publications.getNombre()){
+                        if (autor === autor) {
+                            encontrado = true;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
        /**
         * let arrayArtCient = listaArtCient.filter( c=> c.titulo===titulo)
         * console.log(arrayArtCient[0]);
         */
-   } else if(opcion===4){
-       salir=true;
-   }
-
+ else if(opcion===8){
+    atras = true;
+ }
 }
-   }
+
+
